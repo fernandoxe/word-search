0b5f2009de5b7ddffd7ec5e03eeee5f23ffbfca1
Fernando Parra <fernandoxe@gmail.com>
2021-07-24 13:35:49 -0300

    feat: render lyrics

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,49 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import SearchData from '../../services/searchdata';
+
+const Container = styled.div`
+
+`;
+
 const Search = (props) => {
+  const [inputValue, setInputValue] = useState('');
+  const [result, setResult] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const songs = SearchData.getLyrics(inputValue);
+    console.log(songs);
+    setResult(songs);
+  };
+
+  const parseSongs = (songs) => {
+    return songs.map((song, i) => (
+        <div key={i}>
+          <div>{song.title}</div>
+          {song.lyrics.map((lyric, j) => (
+            <div key={j}>
+              {lyric.map((line, k) => line ? <div key={k}>{line}</div> : null)}
+            </div>
+          ))}
+        </div>
+      )
+    );
+  };
+
   return (
-    <div>Search component</div>
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInputChange} />
+        <button>Search</button>
+        {parseSongs(result)}
+      </form>
+    </Container>
   );
 };
 
