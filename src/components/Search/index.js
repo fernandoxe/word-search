71d7994e1bdf20faa3bf68c9b5085405284b077d
Fernando Parra <fernandoxe@gmail.com>
2021-07-24 14:51:59 -0300

    feat: styles

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,7 +3,28 @@ import styled from 'styled-components';
 import SearchData from '../../services/searchdata';
 
 const Container = styled.div`
+  .card {
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    padding: 8px;
+    margin-bottom: 8px;
+    box-shadow: 2px 2px 5px 1px rgb(0 0 0 / 20%);
+  }
 
+  .song {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    &:not(:last-child) {
+      margin-bottom: 16px;
+    }
+
+    &__title {
+      margin-bottom: 8px;
+    }
+  }
 `;
 
 const Search = (props) => {
@@ -24,10 +45,10 @@ const Search = (props) => {
 
   const parseSongs = (songs) => {
     return songs.map((song, i) => (
-        <div key={i}>
-          <div>{song.title}</div>
+        <div key={i} className="song">
+          <div className="song__title">{song.title}</div>
           {song.lyrics.map((lyric, j) => (
-            <div key={j}>
+            <div key={j} className="card">
               {lyric.map((line, k) => line ? <div key={k}>{line}</div> : null)}
             </div>
           ))}
