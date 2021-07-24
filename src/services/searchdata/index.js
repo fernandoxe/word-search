b729a99218265e2dfde2b22804f479cf48146670
Fernando Parra <fernandoxe@gmail.com>
2021-07-24 13:35:17 -0300

    feat: get lyrics by text

--- src/services/searchdata/index.js
+++ src/services/searchdata/index.js
@@ -1,12 +1,37 @@
 import data from '../../data';
 
 const SearchData = {
-  getArtists: () => data.map(artist => artist.artist),
+  getAllArtists: () => data.map(artist => artist.artist),
   getAllSongs: () => data.map(artist => artist.songs).flat(),
-  getSongsWithText(text) {
+  getSongs(text) {
     const allSongs = this.getAllSongs();
     return allSongs.filter(song => song.lyrics.includes(text));
-  }
+  },
+  songToArrayOfLyrics: (song) => song.lyrics.split('\n'),
+  getLyrics(text) {
+    const songs = this.getSongs(text);
+    const allLyrics = [];
+    for (let i = 0; i < songs.length; i++) {
+      const song = songs[i];
+      const splittedSong = this.songToArrayOfLyrics(song);
+      const lyrics = [];
+      for (let j = 0; j < splittedSong.length; j++) {
+        const line = splittedSong[j];
+        if(line.includes(text)) {
+          lyrics.push([
+            splittedSong[j - 1],
+            splittedSong[j],
+            splittedSong[j + 1],
+          ]);
+        }
+      }
+      allLyrics.push({
+        title: song.title,
+        lyrics,
+      });
+    }
+    return allLyrics;
+  },
 };
 
 export default SearchData;
