e0a829c0439024d051c5d566339d2be051396378
Fernando Parra <fernandoxe@gmail.com>
2021-07-25 01:09:45 -0300

    feat: song component

---
+++ src/components/Song/index.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import Lyrics from '../Lyrics';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  :not(:last-child) {
+    margin-bottom: 2rem;
+  }
+
+  .title {
+    margin-top: 0;
+    margin-bottom: 1rem;
+  }
+`;
+
+const Song = (props) => {
+  return (
+    <Container>
+      <h3 className="title">{props.song.title}</h3>
+      {props.song.lyrics.map((lines, i) => (
+        <Lyrics key={i} lines={lines} />
+      ))}
+    </Container>
+  );
+};
+
+export default Song;
