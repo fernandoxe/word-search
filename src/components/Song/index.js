fe30bd161fc8b1fc3dad4f478e9bd2c37bfadb07
Fernando Parra <fernandoxe@gmail.com>
2021-07-29 02:03:16 -0300

    feat: song styles

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -2,27 +2,29 @@ import styled from 'styled-components';
 import Lyrics from '../Lyrics';
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 2rem;
-  :last-child {
-    margin-bottom: 1rem;
-  }
-
-  .title {
+  > .title {
     margin-top: 0;
     margin-bottom: 1rem;
   }
+
+  > .list {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+
+    > li:not(:last-child) {
+      margin-bottom: 1rem;
+    }
+  }
 `;
 
-const Song = (props) => {
+const Song = props => {
   return (
     <Container>
-      <h3 className="title">{props.song.title}</h3>
-      {props.song.lyrics.map((lines, i) => (
-        <Lyrics key={i} lines={lines} />
-      ))}
+      <h4 className="title">{props.song.title}</h4>
+      <ul className="list">
+        {props.song.lyrics.map((lines, i) => <li key={i}><Lyrics key={i} lines={lines} /></li>)}
+      </ul>
     </Container>
   );
 };
