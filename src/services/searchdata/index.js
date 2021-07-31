bfbab0677589308670b87148c386f6fe30a2c8a4
Fernando Parra <fernandoxe@gmail.com>
2021-07-31 02:13:59 -0300

    feat: optimize methods

--- src/services/searchdata/index.js
+++ src/services/searchdata/index.js
@@ -2,7 +2,6 @@ import data from '../../data';
 
 const SearchData = {
   getAllArtistsNames: () => data.map(artist => artist.artist),
-  getAllSongs: () => data.map(artist => artist.albums).flat().map(album => album.songs).flat(),
   includesText: (text, subtext, ignoreCase = true) => {
     if(ignoreCase) {
       return text.toLowerCase().indexOf(subtext.toLowerCase()) > -1;
@@ -11,51 +10,49 @@ const SearchData = {
     }
   },
   getArtist: artist => data.find(x => x.artist.toLowerCase() === artist.toLowerCase()),
-  getAlbumFromArtist(album, artist) {
-    return this.getArtist(artist)?.albums.find(x => x.name.toLowerCase() === album.toLowerCase());
-  },
-  getAlbumsFromArtist(artist) {
-    return this.getArtist(artist).albums;
-  },
-  getSongsFromArtistAlbum(artist, album) {
-    return this.getAlbumFromArtist(album, artist).songs;
-  },
-  getSongs(text, ignoreCase) {
-    const allSongs = this.getAllSongs();
-    return allSongs.filter(song => this.includesText(song.lyrics, text, ignoreCase));
-  },
   songToArrayOfLyrics: (song) => song.lyrics.split('\n'),
   getAllLyricsWithText(text, ignoreCase) {
-    const artists = this.getAllArtistsNames().map(artist => this.getArtist(artist));
+    return this.getArtistsWithText(text, ignoreCase);
+  },
+  getArtistsWithText(text, ignoreCase) {
+    const artists = data;
     const matchedArtists = [];
     for (let i = 0; i < artists.length; i++) {
-      const artist = artists[i].artist;
-      const albums = this.getAlbumsFromArtist(artist);
-      const matchedAlbums = [];
-      for (let j = 0; j < albums.length; j++) {
-        const album = albums[j].name;
-        const songs = this.getSongsFromArtistAlbum(artist, album);
-        const matchedSongs = [];
-        for (let k = 0; k < songs.length; k++) {
-          const song = songs[k];
-          const matchedLyrics = this.getLyricsFromSongWithText(text, song, ignoreCase);
-          matchedLyrics.length && matchedSongs.push({
-            title: song.title,
-            lyrics: matchedLyrics,
-          });
-        }
-        matchedAlbums.push({
-          name: album,
-          year: albums[j].year,
-          songs: matchedSongs,
-        });
-      }
-      matchedArtists.push({
-        artist: artist,
+      const artist = artists[i];
+      const matchedAlbums = this.getAlbumsFromArtistWithText(text, artist, ignoreCase);
+      matchedAlbums.length && matchedArtists.push({
+        artist: artist.artist,
         albums: matchedAlbums,
       });
     }
     return matchedArtists;
+  },
+  getAlbumsFromArtistWithText(text, artist, ignoreCase) {
+    const albums = artist.albums;
+    const matchedAlbums = [];
+    for (let i = 0; i < albums.length; i++) {
+      const album = albums[i];
+      const matchedSongs = this.getSongsFromAlbumWithText(text, album, ignoreCase);
+      matchedSongs.length && matchedAlbums.push({
+        name: album.name,
+        year: album.year,
+        songs: matchedSongs,
+      });
+    }
+    return matchedAlbums;
+  },
+  getSongsFromAlbumWithText(text, album, ignoreCase) {
+    const songs = album.songs;
+    const matchedSongs = [];
+    for (let i = 0; i < songs.length; i++) {
+      const song = songs[i];
+      const matchedLyrics = this.getLyricsFromSongWithText(text, song, ignoreCase);
+      matchedLyrics.length && matchedSongs.push({
+        title: song.title,
+        lyrics: matchedLyrics,
+      });
+    }
+    return matchedSongs;
   },
   getLyricsFromSongWithText(text, song, ignoreCase) {
     const splittedSong = this.songToArrayOfLyrics(song);
@@ -72,29 +69,15 @@ const SearchData = {
     }
     return matchedLyrics;
   },
-  getLyrics(text, ignoreCase) {
-    const songs = this.getSongs(text, ignoreCase);
-    const allLyrics = [];
-    for (let i = 0; i < songs.length; i++) {
-      const song = songs[i];
-      const splittedSong = this.songToArrayOfLyrics(song);
-      const lyrics = [];
-      for (let j = 0; j < splittedSong.length; j++) {
-        const line = splittedSong[j];
-        if(this.includesText(line, text, ignoreCase)) {
-          lyrics.push([
-            splittedSong[j - 1],
-            splittedSong[j],
-            splittedSong[j + 1],
-          ]);
-        }
-      }
-      allLyrics.push({
-        title: song.title,
-        lyrics,
-      });
-    }
-    return allLyrics;
+  getAllLyricsFromArtistNameWithText(text, artistName, ignoreCase) {
+    const artist = this.getArtist(artistName);
+    const matchedArtists = [];
+    const matchedAlbums = this.getAlbumsFromArtistWithText(text, artist, ignoreCase);
+    matchedAlbums.length && matchedArtists.push({
+      artist: artistName,
+      albums: matchedAlbums,
+    });
+    return matchedArtists;
   },
 };
 
